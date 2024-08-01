11deac34b9db5df29c9b38d83839b22b2a9f8aef
Jazmín <j.gormaz@duocuc.cl>
2024-08-01 12:37:04 -0400

    chaaaao mundo

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main{
     public static void main(String args[]){
         System.out.println("Hola mundo Java!!!" + args[0]);
+        System.out.println("Chaaaaao mundo Java!!!" + args[0]);
     }
 }
