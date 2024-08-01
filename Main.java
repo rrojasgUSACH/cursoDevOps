c3422526f3f8d3759895ac4254e7ce72e91f4d90
Jazmín <j.gormaz@duocuc.cl>
2024-08-01 12:52:54 -0400

    agrega .1

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String args[]){
         System.out.println("Hola mundo Java!!!" + args[0]);
-        System.out.println("Chaaaaao mundo Java21!!!" + args[0]);
+        System.out.println("Chaaaaao mundo Java21.1!!!" + args[0]);
     }
 }
